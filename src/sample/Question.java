e3e357cb1627f6f49ad2a8efe746f07e6a3dd139
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2020-04-07 19:29:22 +0700

    2 question types, question automation 66%

--- src/sample/Question.java
+++ src/sample/Question.java
@@ -11,19 +11,33 @@ public class Question {
     private List<String> answers = new ArrayList<>();
     private String answer;
     private String question;
+    private int type;
+
+
 
     Question(int questionNo){
         try {
             List<String> allLines = Files.readAllLines(Paths.get("./src/assets/question"+questionNo+".txt"));
-            this.answer = allLines.get(4);
-            this.question = allLines.get(0);
-            this.answers.add(allLines.get(1));
-            this.answers.add(allLines.get(2));
-            this.answers.add(allLines.get(3));
-            this.answers.add(allLines.get(4));
+            this.type = Integer.parseInt(allLines.get(0));
+            this.question = allLines.get(1);
+            if (this.type == 1){
+                this.answer = allLines.get(5);
+                this.answers.add(allLines.get(2));
+                this.answers.add(allLines.get(3));
+                this.answers.add(allLines.get(4));
+                this.answers.add(allLines.get(5));
+            }
+            if (this.type == 2){
+                this.answer = allLines.get(2);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public int getType() {
+        return type;
     }
 
     public List<String> getAnswers() {
--- src/sample/gameScreenController.java
+++ src/sample/gameScreenController.java
@@ -14,6 +14,8 @@ import java.util.*;
 
 public class gameScreenController implements Initializable {
 
+    @FXML public Button benar;
+    @FXML public Button salah;
     @FXML private AnchorPane gamePane;
     @FXML private Button kembali;
     @FXML private Button ch1;
@@ -22,20 +24,39 @@ public class gameScreenController implements Initializable {
     @FXML private Button ch4;
     @FXML private Label questionLabel;
 
+
     @FXML private Question question;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.question = new Question(Main.questionNumber);
-        List<String> ansList = this.question.getAnswers();
-        Collections.shuffle(ansList);
+        if(this.question.getType() == 1){
+            List<String> ansList = this.question.getAnswers();
+            Collections.shuffle(ansList);
 
-        ch1.setText(ansList.get(0));
-        ch2.setText(ansList.get(1));
-        ch3.setText(ansList.get(2));
-        ch4.setText(ansList.get(3));
+            ch1.setText(ansList.get(0));
+            ch2.setText(ansList.get(1));
+            ch3.setText(ansList.get(2));
+            ch4.setText(ansList.get(3));
 
-        questionLabel.setText(this.question.getQuestion());
+            questionLabel.setText(this.question.getQuestion());
+
+
+            ch1.setVisible(true);
+            ch2.setVisible(true);
+            ch3.setVisible(true);
+            ch4.setVisible(true);
+
+            ch1.setDisable(false);
+            ch2.setDisable(false);
+            ch3.setDisable(false);
+            ch4.setDisable(false);
+        } else if (this.question.getType() == 2){
+            benar.setVisible(true);
+            benar.setDisable(false);
+            salah.setVisible(true);
+            salah.setDisable(false);
+        }
         Main.questionNumber += 1;
 
     }
@@ -59,6 +80,7 @@ public class gameScreenController implements Initializable {
             }
         } else{
             try {
+                Main.questionNumber = 1;
                 AnchorPane pane = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
                 gamePane.getChildren().setAll(pane);
             } catch (IOException e) {
@@ -98,6 +120,24 @@ public class gameScreenController implements Initializable {
         if (ch1.getText().equals(this.question.getAnswer())){
             System.out.println("correct answer");
         }else {
+            System.out.println("wrong answer");
+        }
+        goToNext();
+    }
+
+    public void klikBenar(){
+        if(this.question.getAnswer().equals("benar")){
+            System.out.println("correct answer");
+        }else {
+            System.out.println("wrong answer");
+        }
+        goToNext();
+    }
+
+    public void klikSalah(){
+        if(this.question.getAnswer().equals("salah")){
+            System.out.println("correct answer");
+        } else {
             System.out.println("wrong answer");
         }
         goToNext();
