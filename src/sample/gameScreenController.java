9097eef0d79c9b380ceae1c05cfa05d554311a66
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2020-04-07 11:23:29 +0700

    gameWindow change works

---
+++ src/sample/gameScreenController.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class gameScreenController implements Initializable {
+
+    @FXML private AnchorPane gamePane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
--- src/sample/mainMenuController.java
+++ src/sample/mainMenuController.java
@@ -1,14 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
-public class mainMenuController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainMenuController implements Initializable {
 
     @FXML private Button mulai;
     @FXML private Button tentangKami;
     @FXML private Button bantuan;
+    @FXML private AnchorPane rootPane;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
+    }
 
+    public void klikMulai(ActionEvent event){
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("gameScreen.fxml"));
+            rootPane.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
