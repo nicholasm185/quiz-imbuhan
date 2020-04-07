4de9205a4c9a1a094da0f73e061101015599199d
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2020-04-07 10:31:11 +0700

    First commit

---
+++ src/sample/mainMenuController.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class mainMenuController {
+
+    @FXML private Button mulai;
+    @FXML private Button tentangKami;
+    @FXML private Button bantuan;
+
+
+
+}
