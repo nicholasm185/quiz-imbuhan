7e436d8d8950ce86946c0b72b727e44250511927
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2020-04-08 16:38:24 +0700

    NMH picture

--- src/sample/tentangKamiController.java
+++ src/sample/tentangKamiController.java
@@ -5,21 +5,36 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class tentangKamiController implements Initializable {
 
-    @FXML
-    private AnchorPane tentangPane;
-    @FXML
-    private Button kembali;
+    @FXML private ImageView nmhPic;
+    @FXML private AnchorPane tentangPane;
+    @FXML private Button kembali;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+
+//            NMH Picture
+            FileInputStream input1 = new FileInputStream("./src/pictures/nmh.jpg");
+            Image image = new Image(input1);
+            nmhPic.setImage(image);
+
+//            TODO: INSERT YOUR PICTURE HERE
+        } catch (FileNotFoundException e) {
+            System.out.println("some pictures were not found");
+        }
+
 
     }
 
